8e0bbc5aa0a7c54fe21fb7e42b3a170f9fc451c4
ks-sup <iiizotovak@gmail.com>
2023-06-03 03:41:55 +0300

    Done

--- predictionImg.js
+++ predictionImg.js
@@ -1,17 +1,45 @@
 function generateM() {
     var afrmt = [ 
    
-    '<img src="images/img_01.png">',
-    '<img src="images/img_02.png">',
-    '<img src="images/img_03.png">',
-    '<img src="images/img_04.png">',
+    '<img src="images/img_01.jpg">',
+    '<img src="images/img_02.jpg">',
+    '<img src="images/img_03.jpg">',
+    '<img src="images/img_04.jpg">',
+    '<img src="images/img_05.jpg">',
+    '<img src="images/img_06.jpg">',
+    '<img src="images/img_07.jpg">',
+    '<img src="images/img_08.jpg">',
+    '<img src="images/img_09.jpg">',
+    '<img src="images/img_10.jpg">',
+    '<img src="images/img_11.jpg">',
+    '<img src="images/img_12.jpg">',
+    '<img src="images/img_13.jpg">',
+    '<img src="images/img_14.jpg">',
+    '<img src="images/img_15.jpg">',
+    '<img src="images/img_16.jpg">',
+    '<img src="images/img_17.jpg">',
+    '<img src="images/img_18.jpg">',
+    '<img src="images/img_19.jpg">',
+    '<img src="images/img_20.jpg">',
+    '<img src="images/img_21.jpg">',
+    '<img src="images/img_22.jpg">',
+    '<img src="images/img_23.jpg">',
+    '<img src="images/img_24.jpg">',
+    '<img src="images/img_25.jpg">',
+    '<img src="images/img_26.jpg">',
+    '<img src="images/img_27.jpg">',
+    '<img src="images/img_28.jpg">',
+    '<img src="images/img_29.jpg">',
+    '<img src="images/img_30.jpg">',
+    
+    
     
     
     
     
 ];
 
-var m=document.querySelector(".div2");
+var m=document.querySelector(".div3");
 m.innerHTML=afrmt[Math.floor(Math.random()*afrmt.length)];
 }
 //style="width: 500px;
